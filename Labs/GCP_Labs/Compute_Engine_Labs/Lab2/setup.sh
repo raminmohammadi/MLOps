197bac2a23d2fef773bf3f9ad72e54e29e078c13
ramin <ramin.mohammadi@tausight.com>
2024-06-13 09:27:55 -0400

    fixed minor indentation issues

--- Labs/GCP_Labs/Compute_Engine_Labs/Lab2/setup.sh
+++ Labs/GCP_Labs/Compute_Engine_Labs/Lab2/setup.sh
@@ -1,7 +1,7 @@
 sudo apt-get update
 sudo apt-get install -y python3 python3-pip python3.11-venv git
 sudo mkdir -p /home/imdb-sentiment-analysis
-sudo chmod -R 777 /home/imdb-sentiment-analysis​​
+sudo chmod -R 777 /home/imdb-sentiment-analysis
 cd /home/imdb-sentiment-analysis
 git clone https://github.com/raminmohammadi/MLOps.git
 cd /home/imdb-sentiment-analysis/MLOps/Labs/GCP_Labs/Compute_Engine_Labs/Lab2
