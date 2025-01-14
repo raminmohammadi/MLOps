e30f6b6e71e5096b4dedec4816b3a23324e1a7c7
raminmohammadi <ramin.mohammadi@tausight.com>
2025-01-14 10:26:27 -0500

    added lab4

--- Labs/Github_Labs/Lab4/tests/test_train_and_save_model.py
+++ Labs/Github_Labs/Lab4/tests/test_train_and_save_model.py
@@ -4,10 +4,10 @@ import pytest
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from unittest.mock import patch, MagicMock
-from train_and_save_model import download_data, preprocess_data, train_model
-from train_and_save_model import get_model_version, update_model_version
-from train_and_save_model import ensure_folder_exists, save_model_to_gcs
-from train_and_save_model import storage
+from Labs.Github_Labs.Lab4.src.train_and_save_model import download_data, preprocess_data, train_model
+from Labs.Github_Labs.Lab4.src.train_and_save_model import get_model_version, update_model_version
+from Labs.Github_Labs.Lab4.src.train_and_save_model import ensure_folder_exists, save_model_to_gcs
+from Labs.Github_Labs.Lab4.src.train_and_save_model import storage
 
 # ----------------- Test Download ----------------- #
 # Test the download_data function to ensure it correctly downloads and returns data
